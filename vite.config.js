562960eba12d26dc2c3f8f0bb7a4fd5f8aa74de0
Juan Diego García <salasjuan@uninorte.edu.co>
2024-09-16 11:53:06 -0500

    added: GitHub pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://thehoodini.github.io/WEB-FE-REACT-HW4-JUAND-GARCIA'
 })
